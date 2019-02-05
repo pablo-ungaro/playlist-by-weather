c24d476dbe853b87f320a7b63ea5bd45ae6be3ef
Pablo Ungaro <ungaro.pablo@gmail.com>
2019-02-05 20:14:21 -0200

    commit inicial gerado pelo spring initializr

---
+++ src/main/java/com/ifood/test/playlistbyweather/PlaylistByWeatherApplication.java
@@ -0,0 +1,14 @@
+package com.ifood.test.playlistbyweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistByWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistByWeatherApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/ifood/test/playlistbyweather/PlaylistByWeatherApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ifood.test.playlistbyweather;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlaylistByWeatherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
